c911b343c542487f500a4fb039276b42e3adf988
Qzimyion <samyakchauhan676@gmail.com>
2024-12-20 02:15:51 +0530

    Updated

--- forge/src/main/java/com/qzimyion/qzibundletweaks/ForgeEvents.java
+++ forge/src/main/java/com/qzimyion/qzibundletweaks/ForgeEvents.java
@@ -1,21 +1,18 @@
 package com.qzimyion.qzibundletweaks;
 
-import com.qzimyion.qzibundletweaks.events.EntityEvents;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.phys.EntityHitResult;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = QzisBundleTweaksConstants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ForgeEvents {
 
-    @SubscribeEvent
-    public static void ItemEntityEvents(PlayerInteractEvent.EntityInteract event){
-        if (event.isCanceled()) return;
-        InteractionResult ForgeRightClickEvent = EntityEvents.bundleStoringItemEntitiesEvent
-                (event.getEntity(), event.getLevel(), event.getHand(), event.getEntity(), new EntityHitResult(event.getEntity()));
-        event.setCanceled(true);
-        event.setCancellationResult(ForgeRightClickEvent);
-    }
+//    @SubscribeEvent
+//    public static void ItemEntityEvents(PlayerInteractEvent.EntityInteract event){
+//        if (event.isCanceled()) return;
+//        BlockPos pos = event.getPos();
+//        BlockEntity blockEntity = event.getLevel().getBlockEntity(pos);
+//        void ForgeRightClickEvent = EntityEvents.bundleStoringItemEntitiesEvent
+//                (event.getLevel(), event.getEntity(), event.getPos(), event.getLevel().getBlockState(event.getPos()), blockEntity);
+//        event.setCanceled(true);
+//        event.setCancellationResult(ForgeRightClickEvent);
+//    }
 }
