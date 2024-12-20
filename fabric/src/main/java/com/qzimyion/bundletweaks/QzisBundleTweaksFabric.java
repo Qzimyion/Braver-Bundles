55b63a33d90ebe5d79c06d0316c42fa67d434822
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-12-20 00:46:20 -0500

    Noelle Please Use A Better Multiloader Template

--- fabric/src/main/java/com/qzimyion/bundletweaks/QzisBundleTweaksFabric.java
+++ fabric/src/main/java/com/qzimyion/bundletweaks/QzisBundleTweaksFabric.java
@@ -2,7 +2,7 @@ package com.qzimyion.bundletweaks;
 
 import net.fabricmc.api.ModInitializer;
 
-public class FabricQzisBundleTweaks implements ModInitializer {
+public class QzisBundleTweaksFabric implements ModInitializer {
     
     @Override
     public void onInitialize() {
---
+++ neoforge/src/main/java/com/qzimyion/bundletweaks/QzisBundleTweaksNeoForge.java
@@ -0,0 +1,13 @@
+package com.qzimyion.bundletweaks;
+
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(QzisBundleTweaksConstants.MOD_ID)
+public class QzisBundleTweaksNeoForge {
+
+    public QzisBundleTweaksNeoForge(IEventBus eventBus) {
+        CommonClass.init();
+    }
+}
