adeb97a7a8e13d821a4c6e331ac8aa70bb2ba95b
Qzimyion <samyakchauhan676@gmail.com>
2024-12-21 01:29:00 +0530

    Removed unneeded comments

--- common/src/main/java/com/qzimyion/bundletweaks/mixin/BundleDecrementFixMixin.java
+++ common/src/main/java/com/qzimyion/bundletweaks/mixin/BundleDecrementFixMixin.java
@@ -14,9 +14,6 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(BlockItem.class)
 public class BundleDecrementFixMixin {
 
-	// Please don't use @Redirect!! It's quite harmful and easily ruins mod compatibility.
-	// Use @WrapOperation instead!
-	// It gives you the method's parameters and lets you either modify what the original method uses, lets you call another method, etc.
 	@WrapOperation(
 		method = "place(Lnet/minecraft/world/item/context/BlockPlaceContext;)Lnet/minecraft/world/InteractionResult;",
 		at = @At(
@@ -25,9 +22,6 @@ public class BundleDecrementFixMixin {
 		)
 	)
 	private void cancelBundleDecrement(ItemStack itemStack, int amount, LivingEntity entity, Operation<Void> original) {
-		// Your original implementation called shrink if it's a Bundle, with an amount of 0... accomplishing nothing.
-		// It also called shrink with 1 if it wasn't! This breaks Creative Mode blocks not shrinking.
-		// I changed this to call the original method instead, or skip if it isn't a Bundle. This way, it works as intended.
 		if (!(itemStack.getItem() instanceof BundleItem)) {
 			original.call(itemStack, amount, entity);
 		}
