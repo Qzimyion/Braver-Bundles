3c6037a6eaf21c584f70f9fd79d03732d9c0a514
Qzimyion <samyakchauhan676@gmail.com>
2025-01-21 11:23:18 +0530

    Removed IEventBus field

--- neoforge/src/main/java/com/qzimyion/braverbundles/neoforge/BraverBundlesNeoForge.java
+++ neoforge/src/main/java/com/qzimyion/braverbundles/neoforge/BraverBundlesNeoForge.java
@@ -4,16 +4,14 @@ import com.qzimyion.braverbundles.BraverBundlesConstants;
 import com.qzimyion.braverbundles.common.BraverBundlesCommon;
 import com.qzimyion.braverbundles.config.CommonModConfig;
 import me.shedaniel.autoconfig.AutoConfig;
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-import org.jetbrains.annotations.NotNull;
 
 @Mod(BraverBundlesConstants.MOD_ID)
 public class BraverBundlesNeoForge {
 
-	public BraverBundlesNeoForge(@NotNull IEventBus eventBus, ModContainer container) {
+	public BraverBundlesNeoForge(ModContainer container) {
 		BraverBundlesCommon.init();
 		container.registerExtensionPoint(IConfigScreenFactory.class, (modContainer, arg) -> AutoConfig.getConfigScreen(CommonModConfig.class, arg).get());
 	}
