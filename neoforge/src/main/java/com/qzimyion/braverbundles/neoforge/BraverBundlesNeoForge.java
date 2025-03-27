7ad653c9c414f65c0e6273c368a90aa02f00f663
Qzimyion <samyakchauhan676@gmail.com>
2025-03-27 14:26:46 +0530

    Re-added midnightlib to neoforge

--- neoforge/src/main/java/com/qzimyion/braverbundles/neoforge/BraverBundlesNeoForge.java
+++ neoforge/src/main/java/com/qzimyion/braverbundles/neoforge/BraverBundlesNeoForge.java
@@ -3,16 +3,19 @@ import com.qzimyion.braverbundles.BraverBundlesClient;
 import com.qzimyion.braverbundles.common.BraverBundlesCommon;
 import com.qzimyion.braverbundles.BraverBundlesConstants;
 import dev.architectury.platform.Platform;
+import eu.midnightdust.lib.config.MidnightConfig;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(BraverBundlesConstants.MOD_ID)
 public final class BraverBundlesNeoForge {
-    public BraverBundlesNeoForge() {
+    public BraverBundlesNeoForge(ModContainer container) {
         BraverBundlesCommon.init();
+        container.registerExtensionPoint(IConfigScreenFactory.class, (modContainer, screen) -> MidnightConfig.getScreen(screen, BraverBundlesConstants.MOD_ID));
         if (Platform.getEnv()==Dist.CLIENT){
             BraverBundlesClient.init();
         }
-
     }
 }
